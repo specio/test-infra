9239f9783cb3093d09ffb40269e674e669d52126
brmclare <brmclare@microsoft.com>
2020-07-05 21:10:02 +0000

    add debian docker files

---
+++ images/debian/validate.sh
@@ -0,0 +1,12 @@
+# OpenEnclave Validation
+git clone --recursive https://github.com/openenclave/openenclave.git
+
+# Build set up
+cd openenclave
+mkdir build
+cd build
+cmake -G "Ninja" ..
+ninja
+
+# Test
+ctest

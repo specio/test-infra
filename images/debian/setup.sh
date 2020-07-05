805c482f4c078cf32abd7c7be7d1cf2427245034
brmclare <brmclare@microsoft.com>
2020-07-05 20:57:19 +0000

    add setup.sh for debian dockers

---
+++ images/debian/setup.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/openenclave/openenclave.git
+
+# Install Ansible
+n=0
+until [ "$n" -ge 5 ]
+do
+   openenclave/scripts/ansible/install-ansible.sh && break
+   n=$((n+1)) 
+   sleep 15
+done
+
+# Run Ansible Playbook
+n=0
+until [ "$n" -ge 5 ]
+do
+   ansible-playbook openenclave/scripts/ansible/oe-contributors-acc-setup.yml && break
+   n=$((n+1)) 
+   sleep 15
+done
+
+# Remove openenclave
+rm -rf openenclave

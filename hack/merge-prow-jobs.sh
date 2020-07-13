55368265833586ee38e7baa7ffd7031909ce3981
Brett McLaren <brmclare@microsoft.com>
2020-07-13 01:42:46 +0000

    add openenclave periodics
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- hack/merge-prow-jobs.sh
+++ hack/merge-prow-jobs.sh
@@ -3,8 +3,6 @@
 # Find mainline prow Config.yaml
 configFile=$PWD/config/prow/config.yaml
 
-# Find prow job yaml configs to merge
-find $PWD/config/jobs -name config.yaml
 
 configArray=$(find $PWD/config/jobs -name *.yaml)
 for config in $configArray

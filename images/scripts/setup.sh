46d1364c57519896744062ef0ca153c2c0dc7a69
Brett McLaren <brmclare@microsoft.com>
2020-07-26 02:14:34 +0000

    add rhel jobs
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/scripts/setup.sh
+++ images/scripts/setup.sh
@@ -1,19 +1,13 @@
-git clone https://github.com/BRMcLaren/openenclave.git
+git clone https://github.com/openenclave/openenclave.git
 
 # Install Ansible
-n=0
-until [ "$n" -ge 5 ]
-do
-   openenclave/scripts/ansible/install-ansible.sh && break
-   n=$((n+1)) 
-   sleep 15
-done
+openenclave/scripts/ansible/install-ansible.sh && break
 
 # Run Ansible Playbook
 n=0
 until [ "$n" -ge 5 ]
 do
-   ansible-playbook openenclave/scripts/ansible/oe-contributors-acc-setup.yml && break
+   ansible-playbook openenclave/scripts/ansible/oe-contributors-acc-setup-no-driver.yml && break
    n=$((n+1)) 
    sleep 15
 done

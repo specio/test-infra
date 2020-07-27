699be41baacedef5f8d8e57c783f2fb1400635a6
Brett McLaren <brmclare@microsoft.com>
2020-07-27 01:00:47 +0000

    retry ansible task
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/scripts/setup.sh
+++ images/scripts/setup.sh
@@ -2,4 +2,10 @@
 images/scripts/ansible/install-ansible.sh && break
 
 # Run Ansible Playbook
-ansible-playbook images/scripts/ansible/oe-linux-ci-setup.yml
+n=0
+until [ "$n" -ge 5 ]
+do
+   ansible-playbook openenclave/scripts/ansible/oe-linux-ci-setup.yml && break
+   n=$((n+1)) 
+   sleep 15
+done

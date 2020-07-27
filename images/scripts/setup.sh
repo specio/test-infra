ce02f79719a84cecf52b5b2509a60c146fbf0af5
Brett McLaren <brmclare@microsoft.com>
2020-07-27 01:12:42 +0000

    add docker tag paths to builds
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/scripts/setup.sh
+++ images/scripts/setup.sh
@@ -1,11 +1,11 @@
 # Install Ansible
-images/scripts/ansible/install-ansible.sh && break
+images/scripts/ansible/install-ansible.sh
 
 # Run Ansible Playbook
 n=0
 until [ "$n" -ge 5 ]
 do
-   ansible-playbook openenclave/scripts/ansible/oe-linux-ci-setup.yml && break
+   ansible-playbook images/scripts/ansible/oe-linux-ci-setup.yml && break
    n=$((n+1)) 
    sleep 15
 done

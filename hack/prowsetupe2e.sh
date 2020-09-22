13ccad1038066953c66636a2c241a5876352becd
Brett McLaren <54328827+BRMcLaren@users.noreply.github.com>
2020-09-22 00:12:47 -0700

    Temp (#79)
    * remove duplicate jobs
    
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>
    
    * change k8's version
    
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- hack/prowsetupe2e.sh
+++ hack/prowsetupe2e.sh
@@ -1,11 +1,11 @@
 export LOCATION="uksouth"
-export RESOURCE_GROUP="OpenEnclaveCICDDev"
+export RESOURCE_GROUP="OpenEnclaveCICD"
 export AKS_CLUSTER_NAME="oe-prow-dev"
 export NODE_SIZE="Standard_DC8_v2"
 export MIN_NODE_COUNT="3"
 export MAX_NODE_COUNT="10"
 export PATH_KEY="~/.ssh/id_rsa.pub"
-export DNS_LABEL="oe-prow-status-dev"
+export DNS_LABEL="oe-prow-status"
 
 # Delete Any Existing Resources
 az group delete --name ${RESOURCE_GROUP} --yes
@@ -28,7 +28,6 @@ az aks create --resource-group ${RESOURCE_GROUP} \
     --min-count ${MIN_NODE_COUNT} \
     --max-count ${MAX_NODE_COUNT} \
     --ssh-key-value ${PATH_KEY} \
-    --kubernetes-version 1.17.7 \
     --aks-custom-headers "usegen2vm=true"
 
 # Get AKS Credentials

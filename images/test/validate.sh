75e5c53c91904613bf202ea03d7a78acb51828f0
Brett McLaren <brmclare@microsoft.com>
2020-10-01 03:04:22 +0000

    add multi compiler validaiton
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/test/validate.sh
+++ images/test/validate.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -ex
 
-# CMake Validation
+# CMake Image Validation
+
 for repo in oeedger8r-cpp openenclave
 do
   # Remove repo if exists
   if [[ -d ./${repo} ]]; then
     rm -rf ./${repo} || sudo rm -rf ./${repo} 
   fi
-  for build in Debug Release RelDebInfo
+
+  git clone --recursive https://github.com/openenclave/${repo}.git && \
+  cd ${repo}
+  # Iterate through supported compilers
+
+  for compiler in clang-7 clang-8
   do
-    git clone --recursive https://github.com/openenclave/${repo}.git && \
-    cd ${repo} && \
-    ../hack/cmake-build.sh --build_package -b=${build} --compiler=clang-7 --hardware_mode && \
-    cd ..
+    for build in Debug Release RelDebInfo
+      do
+        ../hack/cmake-build.sh -b=${build} --compiler=${compiler}--hardware_mode
+      done
   done
   # cd back to root
-  cd ..
+  cd ../..
   # remove repo to save space
   if [[ -d ./${repo} ]]; then
     rm -rf ./${repo} || sudo rm -rf ./${repo} 

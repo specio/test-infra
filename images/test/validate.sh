e75763b2efa60721dff2f161bcbac0381eac5b82
Brett McLaren <brmclare@microsoft.com>
2020-10-01 02:49:52 +0000

    modify validate to cycle through build types
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/test/validate.sh
+++ images/test/validate.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -ex
 
 # CMake Validation
-for repo in  oeedger8r-cpp openenclave
+for repo in oeedger8r-cpp openenclave
 do
+  # Remove repo if exists
   if [[ -d ./${repo} ]]; then
     rm -rf ./${repo} || sudo rm -rf ./${repo} 
   fi
-  git clone --recursive https://github.com/openenclave/${repo}.git && cd ${repo} && exec "../hack/cmake-build.sh" && cd ..
+  for build in Debug Release RelDebInfo
+  do
+    git clone --recursive https://github.com/openenclave/${repo}.git && \
+    cd ${repo} && \
+    ../hack/cmake-build.sh --build_package -b=${build} --compiler=clang-7 --hardware_mode && \
+    cd ..
+  done
+  # cd back to root
+  cd ..
+  # remove repo to save space
+  if [[ -d ./${repo} ]]; then
+    rm -rf ./${repo} || sudo rm -rf ./${repo} 
+  fi
 done

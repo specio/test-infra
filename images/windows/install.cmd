ccbb9a1149be8051e341570f89e9fe58d691ac38
Brett McLaren <brmclare@microsoft.com>
2020-10-01 08:44:03 -0700

    add windows 2016 image
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

---
+++ images/windows/install.cmd
@@ -0,0 +1,2 @@
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Unrestricted -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\images\windows\install-windows-prereqs.ps1 -InstallPath C:\Downloads\prereqs\nuget -LaunchConfiguration SGX1FLC-NoIntelDrivers -DCAPClientType Azure"%1"
+exit /B %ERRORLEVEL%

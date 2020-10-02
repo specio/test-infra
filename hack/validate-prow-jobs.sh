062dadc5268fd13a549b5cb369a9f533faf6e42c
Brett McLaren <brmclare@microsoft.com>
2020-10-02 18:27:08 +0000

    remove validate prow script
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- hack/validate-prow-jobs.sh
+++
@@ -1,7 +0,0 @@
-# We use the  Kubernetes tools
-git clone https://github.com/kubernetes/test-infra/ /prow-tools
-cd /prow-tools
-
-# Validate prow jobs
-set -ex
-bazel-3.0.0 run //prow/cmd/checkconfig -- --plugin-config=$PWD/config/prow/plugins.yaml --config-path=$PWD/config/prow/config.yaml

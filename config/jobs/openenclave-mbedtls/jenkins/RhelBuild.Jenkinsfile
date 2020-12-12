8e68e156b032e249c125bcfe815a8e32564a180e
Brett McLaren <54328827+BRMcLaren@users.noreply.github.com>
2020-12-11 23:40:37 -0800

    reconfigure openenclave mbedtls (#310)
    * reconfigure openenclave mbedtls
    
    Signed-off-by: brmclaren <brmclare@microsoft.com>
    
    * add openenclave-ci jobs
    
    Signed-off-by: brmclaren <brmclare@microsoft.com>

---
+++ config/jobs/openenclave-mbedtls/jenkins/RhelBuild.Jenkinsfile
@@ -0,0 +1,55 @@
+// Pull Request Information
+PULL_NUMBER=env.PULL_NUMBER?env.PULL_NUMBER:"master"
+
+// OS Version Configuration
+LINUX_VERSION=env.LINUX_VERSION?env.LINUX_VERSION:"8"
+
+// Some Defaults
+DOCKER_TAG=env.DOCKER_TAG?env.DOCKER_TAG:"latest"
+COMPILER=env.COMPILER?env.COMPILER:"gcc"
+String[] BUILD_TYPES=['Debug', 'RelWithDebInfo', 'Release']
+
+// Shared library config, check out common.groovy!
+SHARED_LIBRARY="/config/jobs/openenclave-mbedtls/jenkins/common.groovy"
+
+pipeline {
+    options {
+        timeout(time: 60, unit: 'MINUTES') 
+    }
+    agent { label "ACC-RHEL-${LINUX_VERSION}" }
+
+    stages {
+        stage('Checkout'){
+            steps{
+                cleanWs()
+                checkout scm
+            }
+        }
+        stage('Build'){
+            steps{
+                script{
+                    def runner = load pwd() + "${SHARED_LIBRARY}"
+                    for(BUILD_TYPE in BUILD_TYPES){
+                        stage("Ubuntu ${LINUX_VERSION} Build - ${BUILD_TYPE}"){
+                            try{
+                                runner.cleanup()
+                                runner.checkout("${PULL_NUMBER}")
+                                runner.cmakeBuildoeedger8r("${BUILD_TYPE}","${COMPILER}")
+                            } catch (Exception e) {
+                                // Do something with the exception 
+                                error "Program failed, please read logs..."
+                            } finally {
+                                runner.cleanup()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post ('Clean Up'){
+        always{
+            cleanWs()
+        }
+    }
+}
--- config/jobs/openenclave-mbedtls/jenkins/common.groovy
+++ config/jobs/openenclave-mbedtls/jenkins/common.groovy
@@ -1,95 +1,104 @@
-// Common oeedger8r-cpp jenkins functions
-def cmakeBuild( String REPO_NAME ) {
+// Common openenclave-mbedtls jenkins function
 
+/** Checkout openenclave-mbedtls, along with merged pull request. If master is instead passed in, don't check out branch
+  * as this is being ran as a validation of master or as a reverse integration test on the test-infra repo.
+**/
+
+void checkout( String PULL_NUMBER="master" ) {
     if (isUnix()) {
         sh  """
-            cd ${REPO_NAME} && \
-            make && \
-            make check && \
-            make test
-            """
-    } else {
-        bat """
-            uhhh..
-            """
-    }
-}
-
-void checkout( String REPO_NAME ) {
-    if (isUnix()) {
-        sh  """
-            rm -rf ${REPO_NAME} && \
-            git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
-            cd ${REPO_NAME} && \
+            git config --global core.compression 0 && \
+            rm -rf openenclave-mbedtls && \
+            git clone --recursive --depth 1 https://github.com/openenclave/openenclave-mbedtls && \
+            cd openenclave-mbedtls && \
             git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
-            if [[ $PULL_NUMBER -ne 'master' ]]; then
+            if [[ ${PULL_NUMBER} -ne 'master' ]]; then
                 git checkout origin/pr/${PULL_NUMBER}
             fi
             """
     }
     else {
         bat """
-            (if exist ${REPO_NAME} rmdir /s/q ${REPO_NAME}) && \
-            git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
-            cd ${REPO_NAME} && \
+            git config --global core.compression 0 && \
+            (if exist openenclave-mbedtls rmdir /s/q openenclave-mbedtls) && \
+            git clone --recursive --depth 1 https://github.com/openenclave/openenclave-mbedtls && \
+            cd openenclave-mbedtls && \
             git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
             if NOT ${PULL_NUMBER}==master git checkout origin/pr/${PULL_NUMBER}
             """
     }
 }
 
-def ContainerRun(String imageName, String compiler, String task, String runArgs="") {
-    def image = docker.image(imageName)
-    image.pull()
-    image.inside(runArgs) {
-        dir("${WORKSPACE}/openenclave/build") {
-            Run(compiler, task)
+/** Build oeedgr8r based on build config, compiler and platform
+  * TODO: Add container support
+**/
+def cmakeBuildoeedger8r( String BUILD_CONFIG="Release", String COMPILER="clang-7" ) {
+    dir ('openenclave-mbedtls/build') {
+        if (isUnix()) {
+
+            sh  """
+                echo COMPILER IS ${COMPILER}
+                """
+            def c_compiler
+            def cpp_compiler
+            def compiler_version
+            switch(COMPILER) {
+                case "clang-8":
+                    c_compiler = "clang"
+                    cpp_compiler = "clang++"
+                    compiler_version = "8"
+                    break
+                case "clang-7":
+                    c_compiler = "clang"
+                    cpp_compiler = "clang++"
+                    compiler_version = "7"
+                    break
+                case "gcc":
+                    c_compiler = "gcc"
+                    cpp_compiler = "g++"
+
+                    break
+                default:
+                    // This is needed for backwards compatibility with the old
+                    // implementation of the method.
+                    c_compiler = "clang"
+                    cpp_compiler = "clang++"
+                    compiler_version = "8"
+            }
+            if (compiler_version) {
+                c_compiler += "-${compiler_version}"
+                cpp_compiler += "-${compiler_version}"
+            }
+            withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
+                sh  """
+                    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} -Wdev
+                    ninja -v
+                    ctest --output-on-failure --timeout
+                    """
+            }
+        } else {
+            bat """
+                vcvars64.bat x64 && \
+                cmake.exe .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} && \
+                ninja -v -j 4 && \
+                ctest.exe -V --output-on-failure
+                """
         }
     }
 }
 
-def runTask(String task) {
-    dir("${WORKSPACE}/build") {
-        sh """#!/usr/bin/env bash
-                set -o errexit
-                set -o pipefail
-                source /etc/profile
-                ${task}
-            """
-    }
-}
-
-def Run(String compiler, String task, String compiler_version = "") {
-    def c_compiler
-    def cpp_compiler
-    switch(compiler) {
-        case "cross":
-            // In this case, the compiler is set by the CMake toolchain file. As
-            // such, it is not necessary to specify anything in the environment.
-            runTask(task)
-            return
-        case "clang-7":
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "7"
-            break
-        case "gcc":
-            c_compiler = "gcc"
-            cpp_compiler = "g++"
-            break
-        default:
-            // This is needed for backwards compatibility with the old
-            // implementation of the method.
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "8"
-    }
-    if (compiler_version) {
-        c_compiler += "-${compiler_version}"
-        cpp_compiler += "-${compiler_version}"
-    }
-    withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
-        runTask(task);
+// Clean up environment, do not fail on error.
+def cleanup() {
+    if (isUnix()) {
+        try {
+                sh  """
+                    sudo rm -rf openenclave-mbedtls || rm -rf openenclave-mbedtls || echo 'Workspace is clean'
+                    """
+            } catch (Exception e) {
+                // Do something with the exception 
+                error "Program failed, please read logs..."
+            } 
+        
     }
 }
 

716d44a792a0ff3f210d0f60ea73d8d2941d3cee
brmclare <brmclare@microsoft.com>
2021-01-14 08:00:52 +0000

    add custom tagging
    Signed-off-by: brmclare <brmclare@microsoft.com>

--- config/jobs/test-infra-images/jenkins/common.groovy
+++ config/jobs/test-infra-images/jenkins/common.groovy
@@ -2,4 +2,16 @@ String dockerImage(String tag, String dockerfile = ".jenkins/Dockerfile", String
     return docker.build(tag, "${buildArgs} -f ${dockerfile} .")
 }
 
+def get_image_version() {
+    def now = LocalDateTime.now()
+    return (now.format(DateTimeFormatter.ofPattern("yyyy")) + "." + \
+            now.format(DateTimeFormatter.ofPattern("MM")) + "." + \
+            now.format(DateTimeFormatter.ofPattern("dd")))
+}
+
+def get_image_id() {
+    def last_commit_id = sh(script: "git rev-parse --short HEAD", returnStdout: true).tokenize().last()
+    return (get_image_version() + "-" + last_commit_id)
+}
+
 return this

a5a512e24eb39f65d249a9b20a04dd9508676f6c
Szczepan Pecio <szczepan.pecio@intel.com>
2021-01-25 13:13:42 +0100

    Update common.groovy

--- config/jobs/openenclave/jenkins/common.groovy
+++ config/jobs/openenclave/jenkins/common.groovy
@@ -94,6 +94,13 @@ def cmakeBuildopenenclave( String BUILD_CONFIG="Release", String COMPILER="clang
                     whoami
                     pwd
                     ls -la
+                    cd /openenclave
+                    ls -la
+                    """
+                sh  """
+                    whoami
+                    pwd
+                    ls -la
                     cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} ${EXTRA_CMAKE_ARGS} -DLVI_MITIGATION_BINDIR=/usr/local/lvi-mitigation/bin -DCMAKE_INSTALL_PREFIX:PATH='/opt/openenclave' -DCPACK_GENERATOR=DEB -Wdev
                     ninja -v
                     ctest --output-on-failure --timeout

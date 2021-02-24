3094b701405fa3a976e4e13d3bae4d9baecaf663
Szczepan Pecio <szczepan.pecio@intel.com>
2021-02-24 09:56:40 +0100

    added debug strace

--- config/jobs/openenclave/jenkins/common.groovy
+++ config/jobs/openenclave/jenkins/common.groovy
@@ -88,6 +88,9 @@ def cmakeBuildopenenclave( String BUILD_CONFIG="Release", String COMPILER="clang
                 cd ./build
                 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} ${EXTRA_CMAKE_ARGS} -DLVI_MITIGATION_BINDIR=/usr/local/lvi-mitigation/bin -DCMAKE_INSTALL_PREFIX:PATH='/opt/openenclave' -DCPACK_GENERATOR=DEB -Wdev
                 ninja -v
+                cd samples/attestation
+                strace ./host/attestation_host ./enclave/enclave.signed 1>./tLog_AT.txt 2>&1
+                cd ../..
                 ctest --output-on-failure --timeout
                 """
         }

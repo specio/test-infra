ae1587f96d35fa00eae62c4c5f677223bffa3cf5
Szczepan Pecio <szczepan.pecio@intel.com>
2021-03-15 14:51:36 +0100

    Update common.groovy

--- config/jobs/openenclave/jenkins/common.groovy
+++ config/jobs/openenclave/jenkins/common.groovy
@@ -10,10 +10,10 @@ void checkout( String PULL_NUMBER="master" ) {
             git config --global core.compression 0 && \
             rm -rf openenclave && \
             ls -la && \
-            echo $(pwd)/test-infra/config/git-protocol-socks-wrapper && \
-            cat $(pwd)/test-infra/config/git-protocol-socks-wrapper && \
-            chmod +x $(pwd)/test-infra/config/git-protocol-socks-wrapper && \
-            export GIT_PROXY_COMMAND=$(pwd)/test-infra/config/git-protocol-socks-wrapper && \
+            echo ${WORKSPACE}/test-infra/config/git-protocol-socks-wrapper && \
+            cat ${WORKSPACE}/test-infra/config/git-protocol-socks-wrapper && \
+            chmod +x ${WORKSPACE}/test-infra/config/git-protocol-socks-wrapper && \
+            export GIT_PROXY_COMMAND=${WORKSPACE}/test-infra/config/git-protocol-socks-wrapper && \
             git clone --recursive --depth 1 https://github.com/openenclave/openenclave && \
             cd openenclave && \
             git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*

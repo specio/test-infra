1c78a8d53f275c0da0d97899c0c6deffb3baa8a3
Szczepan Pecio <szczepan.pecio@intel.com>
2021-07-06 13:39:25 +0200

    noPub

--- config/jobs/openenclave-intel/jenkins/BuildManager.groovy
+++ config/jobs/openenclave-intel/jenkins/BuildManager.groovy
@@ -1,12 +1,16 @@
 
 def getDockerImage = ["SGX1-FLC": "ImagedoSGXFLC", "SGX1-FLC-KSS": "ImagedoKSS", "SGX1": "ImagedoSGX1"]
+def colors = [red: '#FF0000', green: '#00FF00', blue: '#0000FF']
 
 public void buildAndTest() {
 	if (isUnix()) {
 		sh  """
 			echo "starrt"
 		"""
-		def PLATF=getDockerImage["SGX1-FLC"]
+		for (key in colors.keySet().sort()) {
+		println(key)
+		println(colors[key])
+}
 
 		sh  """
 			echo "mett"

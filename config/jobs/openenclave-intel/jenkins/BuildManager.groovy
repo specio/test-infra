5376a2739e7579170eb9d8c9950066f189d558dc
Szczepan Pecio <szczepan.pecio@intel.com>
2021-07-06 15:17:10 +0200

    verbose

--- config/jobs/openenclave-intel/jenkins/BuildManager.groovy
+++ config/jobs/openenclave-intel/jenkins/BuildManager.groovy
@@ -1,22 +1,33 @@
 
-def getDockerImage = ["SGX1-FLC": "ImagedoSGXFLC", "SGX1-FLC-KSS": "ImagedoKSS", "SGX1": "ImagedoSGX1"]
-
-
-public void buildAndTest() {
-	if (isUnix()) {
-		sh  """
-			echo "starrt"
-		"""
-		def colors = ["SGX1-FLC": "oetools-full-18.04", "SGX1-FLC-KSS": "oetools-full-18.04-KSS", "SGX1": "oetools-sgx1-llc-full-18.04"]
-		for (key in colors.keySet().sort()) {
-			ALA = colors[key]
-			sh  """
-				echo "srala ${ALA}"
-			"""
-		}
-		sh  """
-			echo "mett"
-		"""
-	}
+/** 
+  * Build OpenEnclave and run Tests on specified platform
+**/
+public void buildAndTest(String dockerTag, String compiler, String pullNumber, String oeLogLevel, String specifiedTest) {
+    if (isUnix()) {
+        sh """#!/usr/bin/env bash
+            set -o errexit
+            set -o pipefail
+            echo "======================================================================="
+            echo "Starting Build and Test
+            echo "-----------------------------------------------------------------------"
+            echo "Docker Tag:     ${dockerTag}"
+            echo "Using compiler:    ${compiler}"
+            echo "PullRequest:    ${pullNumber}"
+            echo "OE Log level:      ${oeLogLevel}"
+            echo "CTest test regex:  ${specifiedTest}"
+            """
+        def colors = ["SGX1-FLC": "oetools-full-18.04:", "SGX1-FLC-KSS:${dockerTag}": "oetools-full-18.04-KSS:${dockerTag}", "SGX1": "oetools-sgx1-llc-full-18.04:${dockerTag}"]
+        for (key in colors.keySet()) {
+            currSetUp = colors[key]
+            sh  """
+                echo "Testing: ${currSetUp}"
+            """
+        }
+        sh  """
+            echo "======================================================================="
+            echo "Finished Build and Test
+            echo "-----------------------------------------------------------------------"
+        """
+    }
 }
 return this

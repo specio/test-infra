69de636ef21d83b47f796670fdfb7970016149b7
Szczepan Pecio <szczepan.pecio@intel.com>
2021-07-06 13:32:54 +0200

    noPub

--- config/jobs/openenclave-intel/jenkins/BuildManager.groovy
+++ config/jobs/openenclave-intel/jenkins/BuildManager.groovy
@@ -1,9 +1,17 @@
 
+def getDockerImage = ["SGX1-FLC": "ImagedoSGXFLC", "SGX1-FLC-KSS": "ImagedoKSS", "SGX1": "ImagedoSGX1"]
 
-def buildAndTest() {
+public void buildAndTest() {
 	if (isUnix()) {
 		sh  """
 			echo "starrt"
+		"""
+		for (key in getDockerImage.keySet()) {
+			println(key)
+			println(getDockerImage[key])
+		}
+		sh  """
+			echo "mett"
 		"""
 	}
 }

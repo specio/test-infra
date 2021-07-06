6662b193d40e91211ccc576124d6f1dd7b8e8e08
Szczepan Pecio <szczepan.pecio@intel.com>
2021-07-06 15:30:24 +0200

    verbose

--- config/jobs/openenclave-intel/jenkins/BuildManager.groovy
+++ config/jobs/openenclave-intel/jenkins/BuildManager.groovy
@@ -19,11 +19,17 @@ public void buildAndTest(String dockerTag, String compiler, String pullNumber, S
         def colors = ["SGX1-FLC": "oetools-full-18.04:${dockerTag}", "SGX1-FLC-KSS": "oetools-full-18.04:${dockerTag}", "SGX1": "oetools-sgx1-llc-full-18.04:${dockerTag}"]
         for (key in colors.keySet()) {
             currSetUp = colors[key]
-            sh  """
+            sh """#!/usr/bin/env bash
+                set -o errexit
+                set -o pipefail
                 echo "Testing: ${currSetUp}"
+				def runner = load pwd() + "/test-infra/config/jobs/openenclave-intel/jenkins/common.groovy"
+				runner.checkout("${pullNumber}")
             """
         }
-        sh  """
+        sh """#!/usr/bin/env bash
+            set -o errexit
+            set -o pipefail
             echo "======================================================================="
             echo "Finished Build and Test"
             echo "-----------------------------------------------------------------------"
